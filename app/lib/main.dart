e155122dfb0b1e3ff991eda8d5b1afe42c05121e
palsch <paul@pschwind.de>
2020-10-07 13:23:56 +0200

    add about dialog

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -16,7 +16,7 @@ import 'Timeline.dart';
 void main() {
   EntryModel eModel = EntryModel();
   LicenseRegistry.addLicense(() async* {
-    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    final license = await rootBundle.loadString('fonts/OFL.txt');
     yield LicenseEntryWithLineBreaks(['google_fonts'], license);
   });
   runApp(ChangeNotifierProvider.value(
@@ -57,6 +57,13 @@ class App extends StatelessWidget {
             child: AppBar(
               title: Text("Deins.", style: TextStyle(fontFamily: GoogleFonts.getFont("Crimson Text").fontFamily, fontSize: 36)),
               actions: [
+                IconButton(icon: Icon(Icons.info_outline), onPressed: () => showAboutDialog(
+                  context: context,
+                  children: [
+                    Text("Deins is an app for you to track how you feel about different areas of your life - just by drawing into shapes. The more you draw, the happier you are. You can then get a chart about your developments. There is no syncing into the cloud, everything is kept private on your device.\n\n"),
+                    Text("Impress:\nPaul Schwind\nWiesenweg 27\n97084 Würzburg\nGermany", style: TextStyle(color: Colors.black54))
+                  ]
+                )),
                 IconButton(icon: Icon(Icons.stacked_line_chart_rounded), onPressed: () => _openCharts(context))
               ],
               centerTitle: true
